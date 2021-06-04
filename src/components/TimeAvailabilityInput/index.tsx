db28d460176d57002e2e7f1c0a282db431a7c3ec
Alan Almeida <alanalmeida.emp@gmail.com>
2021-06-04 10:51:02 -0300

    finsh: signup form

--- src/components/TimeAvailabilityInput/index.tsx
+++ src/components/TimeAvailabilityInput/index.tsx
@@ -1,12 +1,20 @@
 import { motion } from 'framer-motion'
 import { FiClock } from 'react-icons/fi'
+import { FieldError } from 'react-hook-form'
 import { Input } from '../Input'
+import { forwardRef, ForwardRefRenderFunction, useEffect } from 'react'
 
 interface TimeAvailabilityInputProps {
-  isOpen?: boolean
+  isOpen?: boolean,
+  errors: FieldError
 }
 
-export function TimeAvailabilityInput({ isOpen }: TimeAvailabilityInputProps) {
+const TimeAvailabilityInputBase:ForwardRefRenderFunction<unknown, TimeAvailabilityInputProps> = ({ isOpen, ...rest }, ref) => {
+
+  // useEffect(() => {
+  //   console.log('rest', ...rest: {})
+  // }, [])
+
   return (
     <motion.div
       animate={isOpen ? {
@@ -26,9 +34,12 @@ export function TimeAvailabilityInput({ isOpen }: TimeAvailabilityInputProps) {
         type="password"
         placeholder="Disponibilidade de Horário"
         icon={<FiClock color="#666360" size={18} />}
+        {...rest}
         // errors={errors.email}
         // {...register('email')}
       />
     </motion.div>
   )
 }
+
+export const TimeAvailabilityInput = forwardRef(TimeAvailabilityInputBase)
--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -5,10 +5,34 @@ import { TimeAvailabilityInput } from '../../components/TimeAvailabilityInput'
 import NextLink from 'next/link'
 import styles from './signup.module.scss'
 import { ActionButton } from '../../components/ActionButton'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+
+type SignUpFormData = {
+  email: string;
+  password: string;
+  name: string;
+  schedule?: string;
+}
+
+const signUpFormSchema = yup.object().shape({
+  name: yup.string().required('Nome obrigatório'),
+  email: yup.string().required('Email obrigatório').email('Email inválido'),
+  password: yup.string().required('Senha obrigatória'),
+  schedule: yup.string().notRequired().default(null).nullable()
+})
 
 export default function SignUp() {
 
   const [userType, setUserType] = useState(true)
+  const { register, handleSubmit, formState: {errors} } = useForm({
+    resolver: yupResolver(signUpFormSchema)
+  })
+
+  const handleSignUp:SubmitHandler<SignUpFormData> = (values) => {
+    console.log(values)
+  }
 
   return (
     <div className={styles.container} >
@@ -23,31 +47,37 @@ export default function SignUp() {
             Sou cabeleireiro
           </button>
         </div>
-        <div className={styles.inputsBox}>
-          <Input
-              type="text"
-              placeholder="Nome"
-              icon={<FiUser color="#666360" size={18} />}
-              // errors={errors.email}
-              // {...register('email')}
-          />
-          <Input
-              type="email"
-              placeholder="E-mail"
-              icon={<FiMail color="#666360" size={18} />}
-              // errors={errors.email}
-              // {...register('email')}
-          />
-          <Input
-              type="password"
-              placeholder="Senha"
-              icon={<FiLock color="#666360" size={18} />}
-              // errors={errors.email}
-              // {...register('email')}
-          />
-          <TimeAvailabilityInput isOpen={!userType} />
-        </div>
-        <ActionButton type="submit" text="Entrar" />
+        <form onSubmit={handleSubmit(handleSignUp)} >
+          <div className={styles.inputsBox}>
+            <Input
+                type="text"
+                placeholder="Nome"
+                icon={<FiUser color="#666360" size={18} />}
+                errors={errors.name}
+                {...register('name')}
+            />
+            <Input
+                type="email"
+                placeholder="E-mail"
+                icon={<FiMail color="#666360" size={18} />}
+                errors={errors.email}
+                {...register('email')}
+            />
+            <Input
+                type="password"
+                placeholder="Senha"
+                icon={<FiLock color="#666360" size={18} />}
+                errors={errors.password}
+                {...register('password')}
+            />
+            <TimeAvailabilityInput
+              isOpen={!userType}
+              errors={errors.schedule}
+              {...register('schedule')}
+            />
+          </div>
+          <ActionButton type="submit" text="Cadastrar" />
+        </form>
         <div className={styles.goBack}>
           <FiArrowLeft color="#F4EDE8" size={18} />
           <NextLink href="/signin">Voltar para login</NextLink>
